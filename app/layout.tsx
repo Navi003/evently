5870bdd71affcd063cc758b70bc744f96fe84d48
Navjot Dhiman <92823851+Navi003@users.noreply.github.com>
2023-12-21 07:24:57 +0100

    added Header with Hero Image

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,7 +2,7 @@ import type { Metadata } from "next";
 import { Poppins } from "next/font/google";
 import "./globals.css";
 import { ClerkProvider } from "@clerk/nextjs";
-
+import Footer from "@/components/shared/Footer";
 const poppins = Poppins({
   subsets: ["latin"],
   weight: ["400", "500", "600", "700"],

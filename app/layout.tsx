b9794d5c016d348f0e16be79443d9f8b07c6b4df
Navjot Dhiman <92823851+Navi003@users.noreply.github.com>
2023-12-25 20:46:01 +0100

    started with MongoDB

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,8 +1,7 @@
+import { ClerkProvider } from "@clerk/nextjs";
 import type { Metadata } from "next";
 import { Poppins } from "next/font/google";
 import "./globals.css";
-import { ClerkProvider } from "@clerk/nextjs";
-import Footer from "@/components/shared/Footer";
 const poppins = Poppins({
   subsets: ["latin"],
   weight: ["400", "500", "600", "700"],
